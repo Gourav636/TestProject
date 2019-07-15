efa36e6c59b5b5f45b582070c24993ac9881f766
Gourav Thakur <gouravthakurnabha@gmail.com>
2019-07-15 17:24:11 +0530

    Commit the DataAccess Changes

---
+++ Test.DataAccess/CoffeeDataProvider.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using Test.DataAccess.Model;
+
+namespace Test.DataAccess
+{
+    public class CoffeeDataProvider
+    {
+        public IEnumerable<CoffeeShop> LoadcoffeeShops()
+        {
+            yield return new CoffeeShop { Location = "Banaglore", BeanStock = 72 };
+            yield return new CoffeeShop { Location = "Mumbai", BeanStock = 52 };
+            yield return new CoffeeShop { Location = "Goa", BeanStock = 12 };
+        }
+    }
+}
---
+++ Test.DataAccess/Model/CoffeeShop.cs
@@ -0,0 +1,10 @@
+﻿
+namespace Test.DataAccess.Model
+{
+    public class CoffeeShop
+    {
+        public string Location { get; set; }
+
+        public int BeanStock { get; set; } 
+    }
+}
